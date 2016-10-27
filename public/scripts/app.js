540d8f84dbc2b7e4f6f595445441a951d8435f4d
Sera Bak <serabak15@gmail.com>
2016-10-27 00:05:43 -0700

    adds ngRoute, layout navbar, main page view, and bootstrap

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,19 @@
-// angular.module('attached', ['ngRoute'])
-// 	.config(config);
 
-angular.module('attached', []);
+angular.module('attached', ['ngRoute', 'ngAnimate', 'ngSanitize','ui.bootstrap'])
+	.config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+
+function config ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/templates/main'
+    }).otherwise({
+      redirectTo: '/'
+    });
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+  });
+}
--- server.js
+++ server.js
@@ -3,13 +3,21 @@ var express = require('express');
 var app = express();
 
 //MIDDLEWARE
-app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/public'));
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+
 
 // var controllers = require('./controllers');
 
 //HTML ENDPOINTS
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/templates/:name', function templates(req, res) {
+  var name = req.params.name;
+  res.sendFile(__dirname + '/views/templates/' + name + '.html');
 });
 
 //JSON ENDPOINTS
